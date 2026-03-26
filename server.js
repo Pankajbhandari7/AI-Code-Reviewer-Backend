e2420e592a0ef896351d697fef23f1b6698bdf1d
Pankaj Bhandari <bhandaripankaj473@gmail.com>
2026-03-26 18:07:35 +0530

    new

---
+++ server.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const app = require('./src/app')
+
+
+const PORT=process.env.PORT||4000;
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+})
+
+
